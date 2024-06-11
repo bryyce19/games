01610b50683810fcff59d992f07a12514fb98538
spring-ford <147882356+spring-ford@users.noreply.github.com>
2024-06-10 23:10:28 -0400

    staticglow

--- 404.js
+++ 404.js
@@ -1,18 +1,18 @@
 document.addEventListener('mousemove', (event) => {
-    const glow = document.querySelector('.glow');
+    const dynamicGlow = document.querySelector('.dynamic-glow');
     const { clientX: x, clientY: y } = event;
 
-    // Set the dimensions of the glow element
+    // Set the dimensions of the dynamic glow element
     const glowWidth = 150;
     const glowHeight = 150;
 
     // Adjust the position so the glow is centered on the mouse pointer
-    glow.style.transform = `translate(${x - glowWidth / 2}px, ${y - glowHeight / 2}px)`;
+    dynamicGlow.style.transform = `translate(${x - glowWidth / 2}px, ${y - glowHeight / 2}px)`;
 });
 
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('.container');
-    const glow = document.createElement('div');
-    glow.classList.add('glow');
-    container.appendChild(glow);
+    const staticGlow = document.createElement('div');
+    staticGlow.classList.add('static-glow');
+    container.appendChild(staticGlow);
 });
